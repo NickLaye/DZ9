508396cd18ec18557a36f4f24538fba53d8e3d59
NickLaye <nicklaye@mail.ru>
2022-05-05 23:02:50 +0300

    Готовое ДЗ 10

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -12,12 +12,37 @@ public class Book {
         this.yearOfPublication = yearOfPublication;
         this.fullName = fullName;
     }
-    public Author getFullName() {return this.fullName;}
+
+    public Author getFullName() {
+        return this.fullName;
+    }
+
     public int getYearOfPublication() {
         return this.yearOfPublication;
     }
-    public String  getNameOfBook() { return this.nameOfBook; }
-    public void setYearOfPublication (int yearOfPublication) {this.yearOfPublication = yearOfPublication;}
 
+    public String getNameOfBook() {
+        return this.nameOfBook;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String toString() {
+        return "Название книги: " + this.nameOfBook + " Автор книги: " + this.fullName + " Год публикации: " + this.yearOfPublication;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) {
+            Book book = (Book) o;
+            return yearOfPublication == book.yearOfPublication;
+        }
+    }
+
+    public int hashCode() {
+        return Book.hashCode(nameOfBook, fullName, yearOfPublication);
+    }
 
 }
